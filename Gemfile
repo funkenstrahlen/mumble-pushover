0fbb40538bdc7d88bb466672b6e2df209957dfca
Stefan Trauth <mail@stefantrauth.de>
2014-07-10 21:40:26 +0200

    basic mumble pushover

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+gem 'pushover'
+gem 'mumble-ruby'
---
+++ mumble-pushover.rb
@@ -0,0 +1,46 @@
+require 'mumble-ruby'
+require 'pushover'
+require 'yaml'
+
+# read config file
+CONFIG = Yaml.load_file("config.yml") unless defined? CONFIG
+
+# configure pushover
+Pushover.configure do |config|
+	config.user = CONFIG['pushover_user_token']
+	config.token = CONFIG['pushover_app_token']
+end
+
+# configure mumble
+cli = Mumble::Client.new('serverdomain') do |config|
+	config.username = CONFIG['mumble_bot_name']
+	config.password = CONFIG['mumble_password']
+end
+
+# connect to mumble
+cli.connect
+cli.mute
+cli.deafen
+# join all channels
+CONFIG['mumble_channel_list'].each do |channel|
+	cli.join_channel(channel)
+end
+
+begin
+	# setup callback on text message and send pushover message on mention
+	cli.on_text_message do |message|
+		# check if user was highlighted
+		if message.include? CONFIG['mumble_username']
+			puts message
+			puts 'sending pushover...'
+			Pushover.notification(message: message, title: 'title')
+		end
+	end
+rescue Interrupt
+	# program quits with CTRL-C
+	cli.disconnect
+	puts "\nexiting..."
+rescue Exception => e
+	puts e
+end
+
