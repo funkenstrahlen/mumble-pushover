f39c199282a120007661f314aa62cfe0f0820e06
Stefan Trauth <mail@stefantrauth.de>
2014-07-13 10:43:56 +0200

    fix waiting for connection. Fix readme (program quit)

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # How to use tldr
 
-Create `config.yml` by copying the `config_example.yml` and applying your settings. To use pushover you have to create a pushover account and a pushover application (to get the app token). Run `bundle install`. Call `ruby mumble-pushover.rb`. Exit by pressing any key.
+Create `config.yml` by copying the `config_example.yml` and applying your settings. To use pushover you have to create a pushover account and a pushover application (to get the app token). Run `bundle install`. Call `ruby mumble-pushover.rb`. Quit by pressing ENTER.
 
 Opus codec required (because mumble-ruby can also stream audio to the mumble server). For Ubuntu you have to install the following packages:
 
--- mumble-pushover.rb
+++ mumble-pushover.rb
@@ -21,30 +21,31 @@ end
 # connect to mumble
 puts 'connecting...'
 mumble.connect
-sleep 2 # wait until connection is established
-puts 'connected to server'
-mumble.mute
-mumble.deafen
-mumble.join_channel(CONFIG['mumble_channel'])
-puts 'joined channel ' + CONFIG['mumble_channel']
 
-# setup callback on text message and send pushover message on mention
-puts 'waiting for messages...'
-mumble.on_text_message do |message|
-	content = message.message
-	# check if user was highlighted
-	if content.include? CONFIG['mumble_username']
-		# send pushover
-		title = 'mumble#' + CONFIG['mumble_channel']
-		puts 'hightlight detected: "' + content + '"'
-		puts 'sending pushover...'
-		Pushover.notification(message: content, title: title)
+# wait for connection
+mumble.on_connected do
+	puts 'connected to server'
+	mumble.me.mute
+	mumble.me.deafen
+	mumble.join_channel(CONFIG['mumble_channel'])
+	puts 'joined channel ' + CONFIG['mumble_channel']
+
+	# setup callback on text message and send pushover message on mention
+	puts 'waiting for messages...'
+	mumble.on_text_message do |message|
+		content = message.message
+		# check if user was highlighted
+		if content.include? CONFIG['mumble_username']
+			# send pushover
+			title = 'mumble#' + CONFIG['mumble_channel']
+			puts 'hightlight detected: "' + content + '"'
+			puts 'sending pushover...'
+			Pushover.notification(message: content, title: title)
+		end
 	end
 end
 
 # handle program exit
-puts 'quit by pressing any key'
-gets
+gets # quit by pressing ENTER
 puts "\nexiting..."
 mumble.disconnect
-
